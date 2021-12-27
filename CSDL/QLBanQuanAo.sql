5d209ab7f6f1d1058952660ccbec4bc2ad3d52e7
ngovanD <ngovan09876@gmail.com>
2021-12-27 08:58:33 +0700

    sua csdl

--- CSDL/QLBanQuanAo.sql
+++ CSDL/QLBanQuanAo.sql
@@ -4,9 +4,15 @@ GO
 USE QLBANQUANAO
 GO
 
+CREATE DATABASE QLBANQUANAO
+GO
+
+USE QLBANQUANAO
+GO
+
 CREATE TABLE TaiKhoan(
 	ID int IDENTITY(1,1) PRIMARY KEY,
-	TenDangNhap varchar(100) UNIQUE,
+	TenDangNhap varchar(100),
 	MatKhau varchar(100),
 	LoaiTaiKhoan BIT, --0: admin 1: nhan vien
 )
@@ -15,13 +21,20 @@ CREATE TABLE NhanVien(
 	MaNhanVien int IDENTITY(1,1) PRIMARY KEY,
 	HoTen nvarchar(100),
 	DiaChi nvarchar(500),
-	SDT char(10),
-	NgayCong int,
-	Luong1Ngay int,
-	Thuong int,
 	ID int,
 	FOREIGN KEY(ID) REFERENCES TaiKhoan(ID) ON DELETE CASCADE ON UPDATE CASCADE
 )
+
+CREATE TABLE Luong(
+	ID_Luong int IDENTITY(1,1) PRIMARY KEY,
+	MaNhanVien int,
+	ThangNam date,
+	SoNgayCong int,
+	Thuong int,
+	LuongCoBanNgay int,
+	FOREIGN KEY(MaNhanVien) REFERENCES NhanVien(MaNhanVien) ON DELETE CASCADE ON UPDATE CASCADE
+)
+
 
 CREATE TABLE KhuyenMai(
 	MaKhuyenMai int IDENTITY(1,1) PRIMARY KEY,
@@ -36,6 +49,7 @@ CREATE TABLE HoaDon(
 	DiaChi nvarchar(500),
 	ThoiGian datetime,
 	LoaiHoaDon BIT, --0: hoa don nhap 1: hoa don xuat
+	TongTien int,
 	ID int,
 	FOREIGN KEY(ID) REFERENCES TaiKhoan(ID) ON DELETE CASCADE ON UPDATE CASCADE
 )
@@ -48,19 +62,36 @@ CREATE TABLE LoaiSanPham(
 CREATE TABLE SanPham(
 	MaSanPham int IDENTITY(1,1) PRIMARY KEY,
 	TenSanPham nvarchar(300),
-	GiaNhap int,
-	GiaXuat int,
-	SoLuongCon int,
+	DonGiaBan int,
+	DonGiaNhap int,
 	MoTa nvarchar(Max),
+	HinhAnh varchar(100),
 	MaLoaiSanPham int,
 	FOREIGN KEY(MaLoaiSanPham) REFERENCES LoaiSanPham(MaLoaiSanPham) ON DELETE CASCADE ON UPDATE CASCADE
 )
 
+CREATE TABLE KichThuoc(
+	ID_KichThuoc int IDENTITY(1,1) PRIMARY KEY,
+	Ten varchar(10)
+)
+
+CREATE TABLE ChiTietSanPham(
+	MaSanPham int,
+	ID_KichThuoc int,
+	SoLuongCon int,
+	FOREIGN KEY(MaSanPham) REFERENCES SanPham(MaSanPham) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY(ID_KichThuoc) REFERENCES KichThuoc(ID_KichThuoc) ON DELETE CASCADE ON UPDATE CASCADE,
+	PRIMARY KEY(MaSanPham, ID_KichThuoc)
+)
+
+
 CREATE TABLE DongHoaDon(
 	MaSanPham int,
+	ID_KichThuoc int,
 	MaHoaDon varchar(13),
 	SoLuong int,
 	FOREIGN KEY(MaSanPham) REFERENCES SanPham(MaSanPham) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY(ID_KichThuoc) REFERENCES KichThuoc(ID_KichThuoc) ON DELETE CASCADE ON UPDATE CASCADE,
 	FOREIGN KEY(MaHoaDon) REFERENCES HoaDon(MaHoaDon) ON DELETE CASCADE ON UPDATE CASCADE,
-	PRIMARY KEY(MaSanPham, MaHoaDon)
+	PRIMARY KEY(MaSanPham, ID_KichThuoc, MaHoaDon)
 )
